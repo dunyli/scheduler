e594fcb2f88b9b94dd1468b895b5781cdf60602a
dunyli <dynuli01@mail.ru>
2026-07-06 22:56:53 +0700

    Реализация Round Robin

---
+++ round_robin.h
@@ -0,0 +1,15 @@
+﻿#ifndef ROUND_ROBIN_H
+#define ROUND_ROBIN_H
+
+#include "scheduler_core.h"
+
+/*
+ * Алгоритм Round Robin (круговая очередь)
+ * - Вытесняющий
+ * - Каждому процессу выделяется квант времени
+ * - После истечения кванта процесс переходит в конец очереди
+ * - Применение: интерактивные системы, системы разделения времени
+ */
+Process** round_robin_scheduler(Scheduler* s);
+
+#endif
